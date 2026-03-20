8f66efd05f9363ec578cd3639da8173da98a38fc
Korejay1111 <157927204+Korejay1111@users.noreply.github.com>
2026-03-20 14:37:08 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -45,9 +45,8 @@ async function fetchCarsFromSanity() {
     }
   }`);
 
- const url = `https://${SANITY_PROJECT_ID}.api.sanity.io/v2024-01-01/data/query/${SANITY_DATASET}?query=${query}`;
-
-  const response = await fetch(url);
+ const url = `https://${SANITY_PROJECT_ID}.apicdn.sanity.io/v2024-01-01/data/query/${SANITY_DATASET}?query=${query}`;
+ 
   if (!response.ok) throw new Error(`Sanity fetch failed: ${response.status}`);
 
   const data = await response.json();
